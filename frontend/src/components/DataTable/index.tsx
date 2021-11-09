c13674b37a79d95e0ad4750df410b07b123b6ef0
mbettcher <mtonon.pmg@gmail.com>
2021-11-08 21:24:02 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,93 @@
+function DataTable() {
+  return (
+    <div>
+      <div className="table-responsive">
+        <table className="table align-middle table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes Visitados</th>
+              <th>Negócios Fechados</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>16043.00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+function Footer() {
+  return (
+    <div className="fixed-bottom">
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">
+            App desenvolvido por{" "}
+            <a
+              href="https://github.com/mbettcher"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Marcelo Tonon Bettcher
+            </a>
+          </p>
+          <p className="text-light">
+            <small>
+              <strong>Semana Spring React</strong>
+              <br />
+              Evento promovido pela escola DevSuperior:{" "}
+              <a
+                href="https://instagram.com/devsuperior.ig"
+                target="_blank"
+                rel="noreferrer"
+              >
+                @devsuperior
+              </a>
+            </small>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
